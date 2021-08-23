a6d64bacba64f42e4ca8a993fdee86b81c5b9f63
simone98dm <simone.dalmas@outlook.it>
2021-08-23 14:59:35 +0200

    Fix missing assets for background, change font, fix deploy

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -1,21 +1,21 @@
 <svelte:options immutable={true} />
 
-<div id="home" class="intro route bg-image">
-	<div class="overlay-itro" />
-	<div class="intro-content display-table">
-		<div class="table-cell">
-			<div class="container">
-				<h1 class="intro-title mb-4">I am Simone</h1>
-				<p class="intro-subtitle animate__animated animate__backInUp">
+<div id='home' class='intro route bg-image'>
+	<div class='overlay-itro'></div>
+	<div class='intro-content display-table'>
+		<div class='table-cell'>
+			<div class='container'>
+				<h1 class='intro-title mb-4'>I am Simone</h1>
+				<p class='intro-subtitle animate__animated animate__backInUp'>
 					<span>I am a frontend & backend developer</span>
 				</p>
 				<p
-					class="intro-subtitle animate__animated animate__backInUp animate__delay-1s"
+					class='intro-subtitle animate__animated animate__backInUp animate__delay-1s'
 				>
 					<span>tech enthusiast</span>
 				</p>
 				<p
-					class="intro-subtitle animate__animated animate__backInUp animate__delay-2s"
+					class='intro-subtitle animate__animated animate__backInUp animate__delay-2s'
 				>
 					<span>🍕 lover</span>
 				</p>
@@ -25,18 +25,18 @@
 </div>
 
 <style scoped>
-	.bg-image {
-		background-image: url(photo-1550745165-9bc0b252726f.jpeg);
-		animation-name: fadeInBackground;
-		animation-duration: 1s;
-	}
+    .bg-image {
+        background-image: url('../../../static/background.jpeg');
+        animation-name: fadeInBackground;
+        animation-duration: 1s;
+    }
 
-	@keyframes fadeInBackground {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
+    @keyframes fadeInBackground {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
 </style>
